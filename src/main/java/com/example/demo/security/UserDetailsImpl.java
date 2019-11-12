e47988ab942f85e03d1924b1b79e8335caca6062
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-11-12 13:56:55 +0000

    Created test suite and test files

--- src/main/java/com/example/demo/security/UserDetailsImpl.java
+++ src/main/java/com/example/demo/security/UserDetailsImpl.java
@@ -1,12 +1,10 @@
 package com.example.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-//import com.example.demo.model.persistence.User;
 import org.springframework.security.core.userdetails.User;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
