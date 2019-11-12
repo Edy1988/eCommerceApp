20f298cf33a7d75403af4b972ae06cc1a544ac66
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-11-12 10:52:19 +0000

    Remove comment

--- src/main/java/com/example/demo/security/UserDetailsImpl.java
+++ src/main/java/com/example/demo/security/UserDetailsImpl.java
@@ -13,13 +13,13 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 import static java.util.Collections.emptyList;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+public class UserDetails implements UserDetailsService {
     
 	@Autowired
 	UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     	com.example.demo.model.persistence.User applicationUser = userRepository.findByUsername(username);
         if (applicationUser == null) {
             throw new UsernameNotFoundException(username);
